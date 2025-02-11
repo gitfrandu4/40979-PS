540c7b4bc7e65f6854a49a22bb447d786125ff46
Francisco J <franlsym@users.noreply.github.com>
2025-02-11 18:27:38 +0000

    Refactor array operations: Introduce Vector class and update sum/difference functions

--- practica1_clean_code.py
+++ practica1_clean_code.py
@@ -8,93 +8,94 @@ Created on Fri Feb 7 09:15:12 2025
 &^Z}@{G2VWD&dY30?QR91^~7NT9Z3q4F
 """
 
-def sum_arrays(array1, array2):
-    """
-    Returns a new list where each element is the sum of the corresponding elements
-    from the given arrays.
-    """
-    result = []
-    
-    return [e1 + e2 for e1, e2 in zip(array1.values, array2.values)]
-
-def difference_arrays(array1, array2):
-    """
-    Returns a new list where each element is the difference of the corresponding elements
-    from the given arrays.
-    """
-    return [e2 - e1 for e1, e2 in zip(array1.values, array2.values)]
-
 class Vector:
     '''
     Class to create the arrays
     '''
     values = []
     
-    def __init__(self, min, max):
-        self.values = [i for i in range(min, max)]
+    def __init__(self, min = None, max = None):
+        """
+        Constructor of the class. It initializes the array with the given values.
+        """
+        if min is None or max is None:
+            self.values = []
+        else:
+            self.values = [i for i in range(min, max)]
         
     def mean(self):
         """
         Returns the mean value of the given array.
         """
         return sum(self.values) / len(self.values)
+    
+    def min(self):
+        """
+        Returns the minimum value of the given array.
+        """
+        return min(self.values)
+    
+    def max(self):
+        """
+        Returns the maximum value of the given array.
+        """
+        return max(self.values)
         
+
+def sum_arrays(array1, array2):
+    """
+    Returns a new list where each element is the sum of the corresponding elements
+    from the given arrays.
+    """
+    result = Vector()
+    
+    for e1, e2 in zip(array1.values, array2.values):
+        result.values.append(e1 + e2)
+    
+    return result
+
+def difference_arrays(array1, array2):
+    """
+    Returns a new list where each element is the difference of the corresponding elements
+    from the given arrays.
+    """
+    result = Vector()
+    
+    for e1, e2 in zip(array1.values, array2.values):
+        result.values.append(e1 - e2)
+
+    return result
       
-# We declare the arrays for the first and second class
+# Declare the arrays for the first and second class
 vector_1 = Vector(0, 100)
 vector_2 = Vector(100, 200)
 
-# Print the content of the arrays
-print(vector_1.values)
-print(vector_2.values)
-
-# Array calculation
+# Array calculations
 sum_result = sum_arrays(vector_1, vector_2)
-difference_result = difference_arrays(vector_1, vector_2)
+diff_result = difference_arrays(vector_2, vector_1)
 
-# Print the result of the sum of the arrays
-print("a1 + a2: ", sum_result)
-print("a1 - a2: ", difference_result)
+# Print the results
+print("a1 = ", vector_1.values, "\n")
+print("a2 = ", vector_2.values, "\n")
+print("a1 + a2: ", sum_result.values, "\n")
+print("a1 - a2: ", diff_result.values, "\n")
+
+# results for the first array
+print("First - Array minimum      =  ", vector_1.min())
+print("First - Array maximum      =  ", vector_1.max())
 print("First - Array mean         =  ", vector_1.mean())
+
+# results for the second array
+print("Second - Array minimum     =  ", vector_2.min())
+print("Second - Array maximum     =  ", vector_2.max())
 print("Second - Array mean        =  ", vector_2.mean())
 
-# We compute the maximum and minimum of the first array by iterating the lists
-minv = 99999 #We initialize the value of the minimum
-maxv = -1
-c = g = 0
+# results for the maximum
+print("Sum - Array minimum        =  ", sum_result.min())
+print("Sum - Array maximum        =  ", sum_result.max())
+print("Sum - Array mean           = ", sum_result.mean())
 
-for i in range(100):
-  if(vector_v1.v1[i]>maxv): maxv = vector_v1.v1[i]
-  if(vector_v1.v1[i]<minv): minv = vector_v1.v1[i]
-  c = c + r[i]
-  g = g + f[i]
-
-# # We show the results for the maximum and minimum 
-# print('First array maximum: ', maxv)
-# print('First array minimum: ', minv)
-# print('Mean of the array with the sum: ', c/100)
-# print('Mean of the array with the difference: ', g/100)
-
-# # We compute the maximum and minimum of the second array by iterating the lists
-# minv = 99999
-# maxv = -1
-# for i in range(100):
-#   if(vector_v2.v2[i]>maxv): maxv = vector_v2.v2[i]
-#   if(vector_v2.v2[i]<minv): minv = vector_v2.v2[i]
-
-# print('Second array maximum: ', maxv)
-# print('Second array minimum: ', minv)
-
-# #We Compute the maximum and minimum of the array with the sum/difference
-# minv = mind = 99999
-# maxv = maxd = -1
-# for i in range(100):
-#   if(r[i]>maxv): maxv = r[i]
-#   if(f[i]>maxd): maxd = f[i]
-#   if(r[i]<minv): minv = r[i]
-#   if(f[i]<mind): mind = f[i]
-
-# print('Maximum of the array with the sum: ', maxv)
-# print('Minimum of the array with the sum: ', minv)
-# print('Maximum of the array with the difference: ', maxd)
-# print('Minimum of the array with the difference: ', mind)
+# results for the minimum
+print("Difference - Array minimum =  ", diff_result.min())
+print("Difference - Array maximum =  ", diff_result.max())
+print("Difference - Array mean    = ", diff_result.mean())
